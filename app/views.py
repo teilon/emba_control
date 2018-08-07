326bf46f952719ced924e675717994917e7fe13e
teilon <teiloncw@gmail.com>
2018-08-07 08:34:41 +0600

    add login page

--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ from .models import Contact
 def index(request):
     context = {}
     return render(request, 'app/index.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'app/login.html', context)
